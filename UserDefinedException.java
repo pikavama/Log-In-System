9b8a781565f50e40b539ab5f24bd48c7775f903a
katleya <piav.macalanda@gmail.com>
2024-03-19 06:32:11 +0800

    Innitial commit, compiler is bugging

---
+++ UserDefinedException.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UserDefinedException {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Type password: ");
+    passwrod = input.nextString();    
+
+    
+}
